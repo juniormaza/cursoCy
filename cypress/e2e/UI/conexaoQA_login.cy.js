68673626bd288b82311f4036a9edfc8373c6c7ea
amari <junior_maza16@hotmail.com>
2024-02-19 16:21:31 -0300

    finalizado os testes de login

--- cypress/e2e/UI/conexaoQA_login.cy.js
+++ cypress/e2e/UI/conexaoQA_login.cy.js
@@ -1,5 +1,9 @@
 describe('página de login', () => {
 
+    const CAMPO_EMAIL = 'login-email'
+    const CAMPO_SENHA = 'login-password'
+    const BOTAO_LOGIN = 'login-submit'
+
     beforeEach(() => {
         cy.visit('/login')
     })
@@ -10,16 +14,16 @@ describe('página de login', () => {
             .as('apiLogin')
 
         // preenche o email 
-        cy.getElement('login-email')
+        cy.getElement(CAMPO_EMAIL)
             .type(Cypress.env('email'))
             
             
         // preenche a senha
-        cy.getElement('login-password')
+        cy.getElement(CAMPO_SENHA)
             .type(Cypress.env('senha'))
 
         // clica no login
-        cy.getElement('login-submit')
+        cy.getElement(BOTAO_LOGIN)
             .click()
          
         // espera a API do login responder
@@ -35,5 +39,50 @@ describe('página de login', () => {
             .and('contain', 'amarildo')
 
     })
-    
+
+    it('Faz o login inválido', () => {
+
+        // spy na API login
+        cy.intercept('POST', '/api/auth')
+            .as('apiLogin')
+
+        // preencher o email
+        cy.getElement(CAMPO_EMAIL)
+            .type(Cypress.env('email'))
+
+        // preencher a senha 
+        cy.getElement(CAMPO_SENHA)
+            .type('abcdef')
+
+        // clicar no botão login
+        cy.getElement(BOTAO_LOGIN)
+            .click()
+
+        // esperar a API
+        cy.wait('@apiLogin')
+            .then(({ response }) => {
+                expect(response.statusCode, 'Validação do HTTP Status Code').to.eq(401)
+            })
+        
+        // validar a mensagem de retorno (erro)
+        cy.getElement('alert')
+            .should('have.text', 'Credenciais inválidas')
+        
+    })
+
+    it.only('valida a digitação de uma email inválido', () => {
+
+        // preencher o emial (inválido)
+        cy.getElement(CAMPO_EMAIL)
+            .type('junior')
+
+        // preencher a senha 
+        cy.getElement(CAMPO_SENHA)
+            .type('abcdef')
+            
+        // validar a máscara do campo
+        cy.contains('p', 'Digite um email válido')
+            .should('be.visible')
+        
+    })
 })
